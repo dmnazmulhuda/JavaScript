c5df3f15f1d31fc2ccb500c2d6ff308bd1ce8cf1
DM Nazmul Huda <45545423+dmnazmulhuda@users.noreply.github.com>
2019-07-17 15:35:50 +0600

    Create Switch Case.js

---
+++ Switch Case.js
@@ -0,0 +1,15 @@
+var day = "Friday";
+
+switch(day) {
+	case "Friday":
+		console.log("This is Holyday");
+		break;
+	case "Saturday":
+		console.log("This is Saturday");
+		break;
+	case "Sunday":
+		console.log("This is Sunday");
+		break;
+	default:
+		console.log("This is default day");
+}
